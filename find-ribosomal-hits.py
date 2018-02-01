e74472b28d780cd97658237c3ce9678107f9df71
jvineis <jvineis@gmail.com>
2018-02-01 08:06:07 -0500

    updated find-ribosomal-hits.py

---
+++ find-ribosomal-hits.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import argparse
+from Bio import SeqIO
+parser = argparse.ArgumentParser(description='''Select sequences from the ncbi-parks-ribosomal.fa that are found in the fasta file of functional gene hits (hmm) to the same genome''')
+parser.add_argument('-phylo' , help = 'The single copy concatenated genes fasta for all 16K reference genomes (if they had them)')
+parser.add_argument('-fa' , help = 'The fasta file of functional genes. We only want sequeuces from "phylo" with headers in this file')
+parser.add_argument('-out', help = 'The output fasta file')
+args = parser.parse_args()
+
+out = open(args.out,'w')
+fa_names = {}
+
+fa = SeqIO.parse(open(args.fa, 'rU'), 'fasta')
+for rec in fa:
+    x = rec.id.split('_')[0]
+    y = rec.id.split('_')[1]
+
+    if x[0] == "G":
+        x_y = x+"_"+y+"_contigs"
+        fa_names[x_y] = rec.id
+    elif x[0] == "D":
+        name = x+"_"+"contigs"
+        fa_names[name] = rec.id
+       
+for key in fa_names.keys():
+    print key
+
+for rec in SeqIO.parse(open(args.phylo, 'rU'), 'fasta'):
+    if rec.id in fa_names.keys():
+        out.write(">"+fa_names[rec.id]+'\n'+str(rec.seq)+'\n')
+#    else:
+#        print rec.id
